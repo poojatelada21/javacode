cf86c5cdb70b30ccb724d1dd532fc65c3bfbf425
pooja telada <poojatelad@gmail.com>
2025-07-24 12:38:07 +0530

    this is my first program

---
+++ sumofnumber.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class sumofnumber{
+public static void main(String args[]){
+int a=0, sum=0;
+while(a<=10){
+sum=sum+a;
+a++;
+}
+System.out.println("sum of number is"+sum);
+}
+}
